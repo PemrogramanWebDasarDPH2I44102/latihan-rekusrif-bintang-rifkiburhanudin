973e67769fb23041b3f078c22a8cdd687482ced9
rifkiburhanudin <42662984+rifkiburhanudin@users.noreply.github.com>
2018-09-13 08:58:27 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,39 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+$bts = 5; 
+function rekursif($i=1){
+	global $bts;
+	echo "*";
+	$i++;
+	if ($i<=$bts){
+		rekursif ($i);
+	}
+	else{
+		echo "<br>";
+		$bts--;
+		if ($bts !=0){
+			rekursif();
+		}
+	}
 }
-rekursif(5);
+	rekursif();
+/////////////////////////////////////////////
+
+echo "<br>";
+function rekursif2($a = 1){
+	global $bts2;
+	echo "$a";
+	$a++;
+	if ($a<=$bts2) {
+	rekursif2($a);
+	}
+	else{
+		echo "<br>";
+		$bts2++;
+		if ($bts2 <= 5) {
+			rekursif2();
+		}
+	}
+}
+rekursif2();
 ?>
+
